d6f2f7e60597a574f0142ec24ad2945c37ca02b7
Ondřej Hošek <ondra.hosek@gmail.com>
2024-12-17 12:01:21 +0100

    page 4 is hard-coded to be the catalog of objects

--- esedump/src/main.rs
+++ esedump/src/main.rs
@@ -25,32 +25,18 @@ fn main() {
         println!("warning: shadow header mismatch");
     }
 
-    let page_1_header = read_page_header(&mut file, &header, 1)
-        .expect("failed to read page 1 header");
-    println!("{:#?}", page_1_header);
+    // page 4 contains the catalog of objects
+    let page_4_header = read_page_header(&mut file, &header, 4)
+        .expect("failed to read page 4 header");
+    println!("{:#?}", page_4_header);
 
-    let page_1_tags = read_page_tags(&mut file, &header, &page_1_header)
-        .expect("failed to read page 1 tags");
-    println!("{:#?}", page_1_tags);
+    let page_4_tags = read_page_tags(&mut file, &header, &page_4_header)
+        .expect("failed to read page 4 tags");
+    println!("{:#?}", page_4_tags);
 
-    let root_page_header_data = read_data_for_tag(&mut file, &header, &page_1_header, &page_1_tags[0])
-        .expect("failed to read root page header data");
-    let root_page_header = read_root_page_header(&root_page_header_data)
-        .expect("failed to parse root page header");
-    println!("{:#?}", root_page_header);
-
-    let space_page_number = root_page_header.space_tree_page_number();
-    let space_page_header = read_page_header(&mut file, &header, space_page_number.into())
-        .expect("failed to read space page header");
-    println!("{:#?}", space_page_header);
-
-    let space_page_tags = read_page_tags(&mut file, &header, &space_page_header)
-        .expect("failed to read space page tags");
-    println!("{:#?}", space_page_tags);
-
-    for space_page_tag in &space_page_tags {
-        let space_tag_data = read_data_for_tag(&mut file, &header, &space_page_header, space_page_tag)
-            .expect("failed to read data");
-        println!("{:#?}", space_tag_data);
+    for page_tag in &page_4_tags {
+        let data = read_data_for_tag(&mut file, &header, &page_4_header, page_tag)
+            .expect("failed to read data for tag");
+        println!("{:?}", data);
     }
 }
