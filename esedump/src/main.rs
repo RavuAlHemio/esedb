3a5fea04ceada5f4ad40f32cacd08aa1e0de3b10
Ondřej Hošek <ondra.hosek@gmail.com>
2024-12-18 20:27:36 +0100

    esedump: trace to stderr, not stdout

--- esedump/src/main.rs
+++ esedump/src/main.rs
@@ -37,7 +37,7 @@ fn main() {
     // set up logging/tracing
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_writer(std::io::stdout)
+        .with_writer(std::io::stderr)
         .pretty()
         .init();
 
